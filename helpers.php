6438ada62eaa916ce483ff83563356ae57735a1f
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-11-26 23:48:46 +0630

    partial create and path helper created

--- helpers.php
+++ helpers.php
@@ -8,6 +8,41 @@
  * @return string
  */
 
-function basePath($path = '') {
+function basePath($path = '')
+{
     return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    if (file_exists(basePath("views/{$name}.view.php"))) {
+        require basePath("views/{$name}.view.php");
+    } else {
+        echo "View {$name} not found";
+    }
+}
+
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+
+    if (file_exists(basePath("views/partials/{$name}.php"))) {
+        require basePath("views/partials/{$name}.php");
+    } else {
+        echo "Partial {$name} not found";
+    }
 }
